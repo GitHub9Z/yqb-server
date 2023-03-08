e87727c029d21d9fbb6368508fe7743fcc7b67e7
周之豪 <zhouzhihao@dangbei.com>
2023-03-08 17:51:50 +0800

    fix

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const route = require('koa-route')
 const serve = require("koa-static")
 const cors = require('koa2-cors')
 const token_check = require('./token_check')
+const bodyParser = require('koa-bodyparser')
 
 Koa.prototype.$route = route
 const app = new Koa()
 Koa.prototype.user_pool = []
+app.use(bodyParser())
 app.use(cors())
 app.use(token_check)
 
